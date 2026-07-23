9d0b32f275fdc8e84b213e65b5cf2d35be4b6cb4
Tanisha0517 <tanisharambadia@gmail.com>
2026-07-23 18:09:27 +0530

    Done

--- Java_Collection/src/com/rays/queue/TestQueue.java
+++ Java_Collection/src/com/rays/queue/TestQueue.java
@@ -7,22 +7,30 @@ public class TestQueue {
 	public static void main(String[] args) {
 
 		Queue q = new ArrayDeque();
+		q.add(1);
+		q.add("Tanisha");
+		q.add('t');
+		q.add(true);
+		q.offer(5);
 
-		for (char c = 'a'; c <= 'z'; c++) {
-			q.offer(c);
-		}
+		System.out.println("Queue : " + q);
 
-		System.out.println("q: " + q);
-//		System.out.println("this is first element: " + q.peek());
-//		System.out.println("q: " + q);
-//		System.out.println("first element to be remove: " + q.poll());
-//		System.out.println("q: " + q);
+		System.out.println("--------------");
+		System.out.println("Remove Tanisha : " + q.remove("Tanisha"));
+		System.out.println(q);
 
-		while (!q.isEmpty()) {
-			System.out.println(q.poll());
-		}
+		System.out.println("--------------");
+		System.out.println("poll()  :" + q.poll());
+		System.out.println(q);
 
-		System.out.println("q: " + q);
+		System.out.println("--------------");
+		System.out.println("element() : " + q.element());
+		System.out.println(q);
+
+		System.out.println("--------------");
+		System.out.println("peek()  : " + q.peek());
+		System.out.println(q);
 
 	}
+
 }
